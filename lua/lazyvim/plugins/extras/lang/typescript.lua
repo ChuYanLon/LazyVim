140b286658865cd1433110f315172bb0da9b2345
chuyanlong <a1008611abcd@126.com>
2025-09-25 15:20:09 +0800

    支持动态

--- lua/lazyvim/plugins/extras/lang/typescript.lua
+++ lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -55,7 +55,7 @@ return {
               suggest = {
                 completeFunctionCalls = true,
               },
-              implicitProjectConfig = {
+              implicitProjectConfiguration = {
                 checkJs = true,
               },
               inlayHints = {
