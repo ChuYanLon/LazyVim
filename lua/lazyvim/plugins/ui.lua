ca5c3c5f323b42fa5ed0bde17018348544546ee1
chuyanlong <a1008611abcd@126.com>
2025-09-28 22:09:53 +0800

    删除配置

--- lua/lazyvim/plugins/ui.lua
+++ lua/lazyvim/plugins/ui.lua
@@ -280,17 +280,6 @@ return {
       toggle = { map = LazyVim.safe_keymap_set },
       words = { enabled = true },
     },
-    -- stylua: ignore
-    keys = {
-      { "<leader>n", function()
-        if Snacks.config.picker and Snacks.config.picker.enabled then
-          Snacks.picker.notifications()
-        else
-          Snacks.notifier.show_history()
-        end
-      end, desc = "Notification History" },
-      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
-    },
   },
 
   {
