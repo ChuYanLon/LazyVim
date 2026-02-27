83224c761e48af5fae7032904811d7cd0f88ec3a
ChuYanLon <65439387+ChuYanLon@users.noreply.github.com>
2026-02-27 08:49:23 +0800

    Add im-select.lua for macOS input method support

---
+++ lua/lazyvim/plugins/extras/util/im-select.lua
@@ -0,0 +1,16 @@
+local function isMacOS()
+  local handle = io.popen("uname -s")
+  if handle then
+    local result = handle:read("*a"):gsub("%s+", "")
+    handle:close()
+    return result == "Darwin"
+  end
+  return false
+end
+
+return {
+    "keaising/im-select.nvim",
+    opts = isMacOS() and {
+      default_command = "im-select",
+    } or {},
+  }
