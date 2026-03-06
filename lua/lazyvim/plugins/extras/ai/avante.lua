3d453973ebe24b8e33adb1da091f370e2c2da675
ChuYanLon <65439387+ChuYanLon@users.noreply.github.com>
2026-03-06 11:49:33 +0800

    Configure Avante provider selection options
    Added selection hint display option for Avante provider.

--- lua/lazyvim/plugins/extras/ai/avante.lua
+++ lua/lazyvim/plugins/extras/ai/avante.lua
@@ -8,6 +8,9 @@ return {
     event = "VeryLazy",
     opts = {
       provider = "copilot",
+      selection = {
+        hint_display = "none",
+      },
       behaviour = {
         auto_set_keymaps = false,
       },
