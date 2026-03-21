b21476e949af114501f1e4c4ed47211a2b0c71f3
ChuYanLon <65439387+ChuYanLon@users.noreply.github.com>
2026-03-21 22:22:13 +0800

    Add Copilot plugin configuration to coc.lua

--- lua/lazyvim/plugins/extras/lang/coc.lua
+++ lua/lazyvim/plugins/extras/lang/coc.lua
@@ -403,6 +403,29 @@ return {
       { "<leader>su",      false },
     },
   },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    event = "BufReadPost",
+    opts = {
+      suggestion = {
+        enabled = not vim.g.ai_cmp,
+        auto_trigger = true,
+        hide_during_completion = vim.g.ai_cmp,
+        keymap = {
+          accept = not vim.g.ai_cmp and "<M-l>" or false, -- handled by nvim-cmp / blink.cmp
+          next = "<M-]>",
+          prev = "<M-[>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
   { "folke/trouble.nvim",        enabled = false },
   { "akinsho/bufferline.nvim",   enabled = false },
   { "nvim-lualine/lualine.nvim", enabled = false },
