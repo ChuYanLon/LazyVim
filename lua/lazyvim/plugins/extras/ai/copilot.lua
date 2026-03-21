ad6e5866ba20d01540485aedbed5841c0dcc64fd
ChuYanLon <65439387+ChuYanLon@users.noreply.github.com>
2026-03-21 22:45:40 +0800

    Refactor Copilot keymap settings in copilot.lua

--- lua/lazyvim/plugins/extras/ai/copilot.lua
+++ lua/lazyvim/plugins/extras/ai/copilot.lua
@@ -8,20 +8,20 @@ return {
     event = "BufReadPost",
     opts = {
       suggestion = {
-        enabled = not vim.g.ai_cmp,
-        auto_trigger = true,
-        hide_during_completion = vim.g.ai_cmp,
-        keymap = {
-          accept = false, -- handled by nvim-cmp / blink.cmp
-          next = "<M-]>",
-          prev = "<M-[>",
+          enabled = not vim.g.ai_cmp,
+          auto_trigger = true,
+          hide_during_completion = vim.g.ai_cmp,
+          keymap = {
+            accept = not vim.g.ai_cmp and "<M-l>" or false, -- handled by nvim-cmp / blink.cmp
+            next = "<M-]>",
+            prev = "<M-[>",
+          },
         },
-      },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
+        panel = { enabled = false },
+        filetypes = {
+          markdown = true,
+          help = true,
+        }
     },
   },
 
