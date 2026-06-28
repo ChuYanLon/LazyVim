c8b5a97bffb07ad1410563325d1d8483e77aebd7
cyl <a1008611abcd@126.com>
2026-06-28 23:20:03 +0800

    refactor(coc): remove renderUi function and inline keymaps into plugin setup The CoC extra has been significantly simplified by removing the renderUi function entirely and inlining the renderKeys keymap definitions directly into the plugin's `config` block. The UI customization (diagnostic signs, highlight groups, completion kind labels, etc.) defined in renderUi was removed to rely on default CoC configuration. Keymaps previously defined in the standalone renderKeys function are now passed directly to create_keys within the main setup, eliminating unnecessary indirection. - Remove `renderUi()` function with all custom UI config, sign definitions, and highlight group links for CoC - Inline `create_keys()` call from `renderKeys()` into plugin config (keeping all keymaps identical) - Remove unused `merge` utility function - Fix minor alignment for `lazydev.nvim` disabled spec

--- lua/lazyvim/plugins/extras/coding/coc.lua
+++ lua/lazyvim/plugins/extras/coding/coc.lua
@@ -9,115 +9,6 @@ local function merge(left, right)
   return result
 end
 
-local function renderUi()
-  local icons = LazyVim.config.icons
-  local diag = icons.diagnostics
-  vim.g.coc_status_error_sign = diag.Error
-  vim.g.coc_status_warning_sign = diag.Warn
-  vim.g.coc_status_info_sign = diag.Info
-  vim.g.coc_status_hint_sign = diag.Hint
-  vim.g.coc_notify_error_icon = diag.Error
-  vim.g.coc_notify_warning_icon = diag.Warn
-  vim.g.coc_notify_info_icon = diag.Info
-  local kind_labels = {
-    text = " " .. icons.kinds.Text .. " ",
-    method = " " .. icons.kinds.Method .. " ",
-    ["function"] = " " .. icons.kinds.Function .. " ",
-    constructor = " " .. icons.kinds.Constructor .. " ",
-    field = " " .. icons.kinds.Field .. " ",
-    variable = " " .. icons.kinds.Variable .. " ",
-    class = " " .. icons.kinds.Class .. " ",
-    interface = " " .. icons.kinds.Interface .. " ",
-    module = " " .. icons.kinds.Module .. " ",
-    property = " " .. icons.kinds.Property .. " ",
-    unit = " " .. icons.kinds.Unit .. " ",
-    value = " " .. icons.kinds.Value .. " ",
-    enum = " " .. icons.kinds.Enum .. " ",
-    keyword = " " .. icons.kinds.Keyword .. " ",
-    snippet = " " .. icons.kinds.Snippet .. " ",
-    color = " " .. icons.kinds.Color .. " ",
-    file = " " .. icons.kinds.File .. " ",
-    reference = " " .. icons.kinds.Reference .. " ",
-    folder = " " .. icons.kinds.Folder .. " ",
-    enumMember = " " .. icons.kinds.EnumMember .. " ",
-    constant = " " .. icons.kinds.Constant .. " ",
-    struct = " " .. icons.kinds.Struct .. " ",
-    event = " " .. icons.kinds.Event .. " ",
-    operator = " " .. icons.kinds.Operator .. " ",
-    typeParameter = " " .. icons.kinds.TypeParameter .. " ",
-    ["default"] = " 󰠱 ",
-  }
-  local config = {
-    diagnostic = {
-      enable = true,
-      virtualText = true,
-      virtualTextCurrentLineOnly = false,
-      floatSource = true,
-      floatPrefix = "●",
-      signText = { Error = diag.Error, Warning = diag.Warn, Info = diag.Info, Hint = diag.Hint },
-      signPriority = 10,
-      refreshAfterInsertMode = true,
-      checkCurrentLine = true,
-    },
-    suggest = {
-      noselect = false,
-      maxCompleteItemCount = 200,
-      detailMaxWidth = 80,
-      floatEnable = true,
-      filterDuplicates = true,
-      removeDuplicateItems = true,
-      formatItems = { "kind", "abbr", "menu", "shortcut" },
-      completionItemKindLabels = kind_labels,
-      pumHeight = vim.o.pumheight,
-    },
-    signature = { enable = true },
-    hover = { border = "rounded" },
-    floating = { border = "rounded" },
-    codeLens = { enable = true },
-    inlayHint = { enable = true },
-    tree = { renderChildren = true, openedIcon = "", closedIcon = "" },
-  }
-  vim.g.coc_user_config = vim.tbl_deep_extend("force", vim.g.coc_user_config or {}, config)
-  local sign = vim.fn.sign_define
-  sign("DiagnosticSignError", { text = diag.Error, texthl = "DiagnosticSignError" })
-  sign("DiagnosticSignWarn", { text = diag.Warn, texthl = "DiagnosticSignWarn" })
-  sign("DiagnosticSignInfo", { text = diag.Info, texthl = "DiagnosticSignInfo" })
-  sign("DiagnosticSignHint", { text = diag.Hint, texthl = "DiagnosticSignHint" })
-  local hl = vim.api.nvim_set_hl
-  local function link(name, linkto)
-    hl(0, name, { link = linkto })
-  end
-  link("CocFloating", "NormalFloat")
-  link("CocFloatBorder", "FloatBorder")
-  link("CocFloatActive", "NormalFloat")
-  link("CocMenuSel", "PmenuSel")
-  link("CocPumSearch", "PmenuSel")
-  link("CocPumDetail", "Comment")
-  link("CocHighlightText", "Visual")
-  link("CocCodeLens", "Comment")
-  link("CocInlayHint", "Comment")
-  link("CocBold", "Bold")
-  link("CocItalic", "Italic")
-  link("CocUnderline", "Underlined")
-  link("CocMarkdownLink", "markdownLinkText")
-  link("CocMarkdownHeader", "markdownH1")
-  link("CocMarkdownCode", "markdownCode")
-  link("CocErrorHighlight", "DiagnosticUndercurlError")
-  link("CocWarningHighlight", "DiagnosticUndercurlWarn")
-  link("CocInfoHighlight", "DiagnosticUndercurlInfo")
-  link("CocHintHighlight", "DiagnosticUndercurlHint")
-  link("CocDeprecatedHighlight", "DiagnosticDeprecated")
-  link("CocUnusedHighlight", "DiagnosticUnnecessary")
-  link("CocErrorVirtualText", "DiagnosticVirtualTextError")
-  link("CocWarningVirtualText", "DiagnosticVirtualTextWarn")
-  link("CocInfoVirtualText", "DiagnosticVirtualTextInfo")
-  link("CocHintVirtualText", "DiagnosticVirtualTextHint")
-  link("CocErrorSign", "DiagnosticSignError")
-  link("CocWarningSign", "DiagnosticSignWarn")
-  link("CocInfoSign", "DiagnosticSignInfo")
-  link("CocHintSign", "DiagnosticSignHint")
-end
-
 local function create_keys(maps, opts)
   opts = opts or {}
   for _, map in pairs(maps) do
@@ -134,44 +25,6 @@ local function create_keys(maps, opts)
   end
 end
 
-local function renderKeys()
-  create_keys({
-    { "i", "<C-j>",      'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', { silent = true, noremap = true, expr = true, replace_keycodes = false } },
-    { "i", "<C-k>",      [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]],                                         { silent = true, noremap = true, expr = true, replace_keycodes = false } },
-    { "i", "<Cr>",       [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],       { silent = true, noremap = true, expr = true, replace_keycodes = false } },
-    { "n", "K",          '<CMD>lua _G.show_docs()<CR>',                                                              { silent = true } },
-    { "i", "<c-i>",      "coc#refresh()",                                                                            { silent = true, expr = true } },
-    { 'n', 'gd',         '<Plug>(coc-definition)',                                                                   { desc = 'Go to definition' } },
-    { 'n', 'gy',         '<Plug>(coc-type-definition)',                                                              { desc = 'Go to type definition' } },
-    { 'n', 'gi',         '<Plug>(coc-implementation)',                                                               { desc = 'Go to implementation' } },
-    { 'n', 'gr',         '<Plug>(coc-references)',                                                                   { desc = 'Find references' } },
-    { 'n', '<leader>cr', '<Plug>(coc-codeaction-refactor)',                                                          { desc = 'refactor' } },
-    { 'x', '<leader>cr', '<Plug>(coc-codeaction-refactor-selected)',                                                 { desc = 'refactor' } },
-    { 'n', '<leader>cf', ':Format<CR>',                                                                              { desc = 'format' } },
-    { 'x', '<leader>cf', '<Plug>(coc-format-selected)',                                                              { desc = 'format' } },
-    { 'n', '<leader>ca', '<Plug>(coc-codeaction-line)',                                                              { desc = 'action' } },
-    { 'x', '<leader>ca', '<Plug>(coc-codeaction-selected)',                                                          { desc = 'action' } },
-    { 'n', '<leader>cA', '<Plug>(coc-codeaction-source)',                                                            { desc = 'sourceAction' } },
-    { 'n', '<leader>cl', '<Plug>(coc-codelens-action)',                                                              { desc = 'codelensAction' } },
-    { 'n', '<leader>cn', '<Plug>(coc-rename)',                                                                       { desc = 'rename' } },
-    { 'n', '<space>cs',  ':<C-u>CocOutline<CR>',                                                                     { desc = 'outline' } },
-    { 'n', '<leader>cq', '<Plug>(coc-fix-current)',                                                                  { desc = 'fix' } },
-    { 'n', '<space>xx',  ':<C-u>CocDiagnostics<CR>',                                                                 { desc = 'diagnostics' } },
-    { 'n', '<space>xs',  ':<C-u>CocList diagnostics<CR>',                                                            { desc = 'all diagnostics' } },
-    { 'n', '[d',         '<Plug>(coc-diagnostic-prev)',                                                              { desc = 'previous diagnostic' } },
-    { 'n', ']d',         '<Plug>(coc-diagnostic-next)',                                                              { desc = 'next diagnostic' } },
-    { 'n', '<leader>cm', ':<C-u>CocCommand loader.open<CR>',                                                               { desc = 'commands' } },
-    { 'n', '<C-n>',      'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-n>"',                                   { expr = true, desc = 'Scroll down in Coc float' } },
-    { 'n', '<C-p>',      'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-p>"',                                   { expr = true, desc = 'Scroll up in Coc float' } },
-    { 'i', '<C-n>',      'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"',                     { expr = true, desc = 'Insert mode: Scroll down float' } },
-    { 'i', '<C-p>',      'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"',                      { expr = true, desc = 'Insert mode: Scroll up float' } },
-    { 'v', '<C-n>',      'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-n>"',                                   { expr = true, desc = 'Visual mode: Scroll down float' } },
-    { 'v', '<C-p>',      'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-p>"',                                   { expr = true, desc = 'Visual mode: Scroll up float' } },
-    { 'n', 'mm',         '<Plug>(coc-translator-p)',                                                                 { desc = 'translate' } },
-    { 'v', 'mm',         '<Plug>(coc-translator-pv)',                                                                { desc = 'translate' } },
-  })
-end
-
 return {
   { "neovim/nvim-lspconfig",  enabled = false },
   { "hrsh7th/nvim-cmp",       enabled = false },
@@ -181,7 +34,7 @@ return {
   { "mfussenegger/nvim-lint", enabled = false },
   { "catppuccin/nvim",        enabled = false },
   { "mason-org/mason.nvim",   enabled = false },
-  { "folke/lazydev.nvim",   enabled = false },
+  { "folke/lazydev.nvim",     enabled = false },
   {
     "nvim-lualine/lualine.nvim",
     opts = function(_, opts)
@@ -329,8 +182,41 @@ return {
         command = "setl formatexpr=CocAction('formatSelected')",
         desc = "Setup formatexpr specified filetype(s)."
       })
-      renderUi()
-      renderKeys()
+      create_keys({
+        { "i", "<C-j>",      'coc#pum#visible() ? coc#pum#next(1) : v:lua.check_back_space() ? "<TAB>" : coc#refresh()', { silent = true, noremap = true, expr = true, replace_keycodes = false } },
+        { "i", "<C-k>",      [[coc#pum#visible() ? coc#pum#prev(1) : "\<C-h>"]],                                         { silent = true, noremap = true, expr = true, replace_keycodes = false } },
+        { "i", "<Cr>",       [[coc#pum#visible() ? coc#pum#confirm() : "\<C-g>u\<CR>\<c-r>=coc#on_enter()\<CR>"]],       { silent = true, noremap = true, expr = true, replace_keycodes = false } },
+        { "n", "K",          '<CMD>lua _G.show_docs()<CR>',                                                              { silent = true } },
+        { "i", "<c-i>",      "coc#refresh()",                                                                            { silent = true, expr = true } },
+        { 'n', 'gd',         '<Plug>(coc-definition)',                                                                   { desc = 'Go to definition' } },
+        { 'n', 'gy',         '<Plug>(coc-type-definition)',                                                              { desc = 'Go to type definition' } },
+        { 'n', 'gi',         '<Plug>(coc-implementation)',                                                               { desc = 'Go to implementation' } },
+        { 'n', 'gr',         '<Plug>(coc-references)',                                                                   { desc = 'Find references' } },
+        { 'n', '<leader>cr', '<Plug>(coc-codeaction-refactor)',                                                          { desc = 'refactor' } },
+        { 'x', '<leader>cr', '<Plug>(coc-codeaction-refactor-selected)',                                                 { desc = 'refactor' } },
+        { 'n', '<leader>cf', ':Format<CR>',                                                                              { desc = 'format' } },
+        { 'x', '<leader>cf', '<Plug>(coc-format-selected)',                                                              { desc = 'format' } },
+        { 'n', '<leader>ca', '<Plug>(coc-codeaction-line)',                                                              { desc = 'action' } },
+        { 'x', '<leader>ca', '<Plug>(coc-codeaction-selected)',                                                          { desc = 'action' } },
+        { 'n', '<leader>cA', '<Plug>(coc-codeaction-source)',                                                            { desc = 'sourceAction' } },
+        { 'n', '<leader>cl', '<Plug>(coc-codelens-action)',                                                              { desc = 'codelensAction' } },
+        { 'n', '<leader>cn', '<Plug>(coc-rename)',                                                                       { desc = 'rename' } },
+        { 'n', '<space>cs',  ':<C-u>CocOutline<CR>',                                                                     { desc = 'outline' } },
+        { 'n', '<leader>cq', '<Plug>(coc-fix-current)',                                                                  { desc = 'fix' } },
+        { 'n', '<space>xx',  ':<C-u>CocDiagnostics<CR>',                                                                 { desc = 'diagnostics' } },
+        { 'n', '<space>xs',  ':<C-u>CocList diagnostics<CR>',                                                            { desc = 'all diagnostics' } },
+        { 'n', '[d',         '<Plug>(coc-diagnostic-prev)',                                                              { desc = 'previous diagnostic' } },
+        { 'n', ']d',         '<Plug>(coc-diagnostic-next)',                                                              { desc = 'next diagnostic' } },
+        { 'n', '<leader>cm', ':<C-u>CocCommand loader.open<CR>',                                                         { desc = 'commands' } },
+        { 'n', '<C-n>',      'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-n>"',                                   { expr = true, desc = 'Scroll down in Coc float' } },
+        { 'n', '<C-p>',      'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-p>"',                                   { expr = true, desc = 'Scroll up in Coc float' } },
+        { 'i', '<C-n>',      'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(1)<cr>" : "<Right>"',                     { expr = true, desc = 'Insert mode: Scroll down float' } },
+        { 'i', '<C-p>',      'coc#float#has_scroll() ? "<c-r>=coc#float#scroll(0)<cr>" : "<Left>"',                      { expr = true, desc = 'Insert mode: Scroll up float' } },
+        { 'v', '<C-n>',      'coc#float#has_scroll() ? coc#float#scroll(1) : "<C-n>"',                                   { expr = true, desc = 'Visual mode: Scroll down float' } },
+        { 'v', '<C-p>',      'coc#float#has_scroll() ? coc#float#scroll(0) : "<C-p>"',                                   { expr = true, desc = 'Visual mode: Scroll up float' } },
+        { 'n', 'mm',         '<Plug>(coc-translator-p)',                                                                 { desc = 'translate' } },
+        { 'v', 'mm',         '<Plug>(coc-translator-pv)',                                                                { desc = 'translate' } },
+      })
     end,
   },
 }
