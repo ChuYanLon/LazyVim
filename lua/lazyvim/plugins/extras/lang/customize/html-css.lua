2bdd44126c13d01e95aa0a31c153147cdf9269ab
cyl <a1008611abcd@126.com>
2026-07-22 14:43:46 +0800

    feat(lang/html-css): refactor config with recommended() and improved LSP settings - Add `recommended()` function for conditional loading based on filetypes and root markers - Replace local `list_insert_unique` with shared `require("utils").list_insert_unique` - Move LSP config after mason setup and reorder plugin declarations - Add `cssls` lint settings to ignore `unknownAtRules` for css, less, and scss - Disable scss validation in cssls - Remove emmet_language_server configuration and custom filetype mappings - Drop "styled" from treesitter ensure_installed list - Add `cssmodules-language-server` to mason ensure_installed - Reorganize code with clearer section comments

--- lua/lazyvim/plugins/extras/lang/customize/html-css.lua
+++ lua/lazyvim/plugins/extras/lang/customize/html-css.lua
@@ -1,78 +1,27 @@
-local function list_insert_unique(list, items)
-  local set = {}
-  for _, item in ipairs(list) do
-    set[item] = true
-  end
-  for _, item in ipairs(items) do
-    if not set[item] then
-      table.insert(list, item)
-      set[item] = true
-    end
-  end
-  return list
-end
-
-local filetypes = {
-  "css",
-  "eruby",
-  "html",
-  "htmldjango",
-  "javascriptreact",
-  "less",
-  "pug",
-  "sass",
-  "scss",
-  "typescriptreact",
-  "vue",
-}
-
-vim.filetype.add({
-  pcss = "postcss",
-  postcss = "postcss",
-})
-
 return {
+  recommended = function()
+    return LazyVim.extras.wants({
+      ft = { "css", "html" },
+      root = { "*.html", "*.css", "*.less", "*.scss" },
+    })
+  end,
   {
     "nvim-treesitter/nvim-treesitter",
     optional = true,
     opts = function(_, opts)
       if opts.ensure_installed ~= "all" then
-        opts.ensure_installed = list_insert_unique(opts.ensure_installed, { "html", "css", "scss", "styled" })
+        opts.ensure_installed = require("utils").list_insert_unique(opts.ensure_installed, { "html", "css", "scss" })
       end
       vim.treesitter.language.register("scss", "less")
       vim.treesitter.language.register("scss", "postcss")
     end,
   },
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        html = { init_options = { provideFormatter = false } },
-        cssls = { init_options = { provideFormatter = false } },
-        emmet_language_server = {
-          init_options = {
-            --- @type boolean Defaults to `true`
-            showAbbreviationSuggestions = false,
-            --- @type "always" | "never" Defaults to `"always"`
-            showExpandedAbbreviation = "always",
-            --- @type boolean Defaults to `false`
-            showSuggestionsAsSnippets = true,
-          },
-          filetypes,
-        },
-      },
-    },
-  },
+  -- Linters & formatters
   {
     "mason-org/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      list_insert_unique(opts.ensure_installed, {
-        "html-lsp",
-        "css-lsp",
-        "emmet-language-server",
-      })
-    end,
+    opts = {
+      ensure_installed = { "html-lsp", "cssmodules-language-server", "css-lsp", "emmet-language-server" },
+    },
   },
   {
     "nvim-mini/mini.icons",
@@ -80,6 +29,35 @@ return {
     opts = {
       filetype = {
         postcss = { glyph = "󰌜", hl = "MiniIconsOrange" },
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        html = { init_options = { provideFormatter = false } },
+        cssls = {
+          init_options = { provideFormatter = false },
+          settings = {
+            css = {
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            less = {
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+            scss = {
+              validate = false,
+              lint = {
+                unknownAtRules = "ignore",
+              },
+            },
+          },
+        },
       },
     },
   },
